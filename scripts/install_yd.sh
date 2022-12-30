3e9533bd59929291a3b70ffdbb8dcd2c3820f37e
sunder <evenquantity@gmail.com>
2022-12-30 16:26:13 +0300

    Fix yandex disk install

--- scripts/install_yd.sh
+++ scripts/install_yd.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
 mkdir temp
 cd temp
 wget http://repo.yandex.ru/yandex-disk/yandex-disk-latest.x86_64.rpm
-sudo rpm -i yandex-disk-latest.x86_64.rpm
+ls -la
+pkexec sh -c "rpm -ihv $PWD/yandex-disk-latest.x86_64.rpm"
+cd ..
+rm -rf temp/
