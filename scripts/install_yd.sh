20ad6928bd3660180519bd635cbf1c2b39db4398
sunder <evenquantity@gmail.com>
2022-12-31 15:31:50 +0300

    Fix yandex disk install

--- scripts/install_yd.sh
+++ scripts/install_yd.sh
@@ -3,10 +3,10 @@ mkdir temp
 
 if [[ $(which rpm) == "/usr/bin/rpm" ]]; then
     wget http://repo.yandex.ru/yandex-disk/yandex-disk-latest.x86_64.rpm -P $PWD/temp
-    pkexec sh -c "rpm -i $PWD/yandex-disk-latest.x86_64.rpm"
+    pkexec sh -c "rpm -i $PWD/temp/yandex-disk-latest.x86_64.rpm"
 elif [[ $(which apt) == "/usr/bin/apt" ]]; then
     wget http://repo.yandex.ru/yandex-disk/yandex-disk_latest_amd64.deb -P $PWD/temp
-    pkexec sh -c "dpkg -i $PWD/yandex-disk-latest_amd64.deb"
+    pkexec sh -c "dpkg -i $PWD/temp/yandex-disk-latest_amd64.deb"
 fi
 
 rm -rf temp/
