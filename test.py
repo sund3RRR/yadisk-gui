44aaafd39e1dfaadc26bc39c4125ac4d7acd3dfd
sunder <evenquantity@gmail.com>
2023-01-14 17:35:34 +0300

    Fix auth test script

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import subprocess, time
 
-yd = subprocess.Popen(["yandex-disk", "token"],
+yd = subprocess.Popen(["yandex-disk"],
                 stdin=subprocess.PIPE,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
                 universal_newlines=True,
-                bufsize=20)
+                bufsize=0)
+stdout = yd.communicate("token")
 while yd.poll() == None:
-    print(yd.stdout.readline())
+    print(stdout)
     time.sleep(0.5)
 
 print(yd.poll())
